66c850c2e71d3598b59b6fe02662701d8197dba5
Javeriya Imran <121876323+jennylewis45@users.noreply.github.com>
2023-08-27 20:54:09 +0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,47 +1,76 @@
-// // Initialize Firebase (replace with your own Firebase config)
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBWhG-eLcO09apFi7r1I-QqWkksLcTZAOs",
-//     authDomain: "saylani-web-596d5.firebaseapp.com",
-//     databaseURL: "https://saylani-web-596d5-default-rtdb.firebaseio.com",
-//     projectId: "saylani-web-596d5",
-//     storageBucket: "saylani-web-596d5.appspot.com",
-//     messagingSenderId: "34846311971",
-//     appId: "1:34846311971:web:7fff13c80a185c97c65396",
-//     measurementId: "G-KTX6VEHJ4F"
-//   };
-//   firebase.initializeApp(firebaseConfig);
-//   const db = firebase.firestore();
-  
-//   // Get references to HTML elements
-//   const blogForm = document.getElementsByClassName("blogForm");
-//   const titleInput = document.getElementById("title");
-//   const bodyInput = document.getElementById("body");
-  
-//   // Event listener for form submission
-//   blogForm.addEventListener("submit", function (event) {
-//     event.preventDefault(); // Prevent the default form submission
-  
-//     // Get values from form inputs
-//     const title = titleInput.value;
-//     const body = bodyInput.value;
-  
-//     // Check if the title and body meet your validation requirements here
-  
-//     // Create a new blog post in Firestore
-//     db.collection("blogs")
-//       .add({
-//         title: title,
-//         body: body,
-//         timestamp: firebase.firestore.FieldValue.serverTimestamp()
-//       })
-//       .then(function (docRef) {
-//         console.log("Blog post added with ID: ", docRef.id);
-//         // Clear the form after successful submission
-//         titleInput.value = "";
-//         bodyInput.value = "";
-//       })
-//       .catch(function (error) {
-//         console.error("Error adding blog post: ", error);
-//       });
-//   });
-  
+    // Initialize Firebase
+    const firebaseConfig = {
+        apiKey: "AIzaSyBWhG-eLcO09apFi7r1I-QqWkksLcTZAOs",
+        authDomain: "saylani-web-596d5.firebaseapp.com",
+        databaseURL: "https://saylani-web-596d5-default-rtdb.firebaseio.com",
+        projectId: "saylani-web-596d5",
+        storageBucket: "saylani-web-596d5.appspot.com",
+        messagingSenderId: "34846311971",
+        appId: "1:34846311971:web:7fff13c80a185c97c65396",
+        measurementId: "G-KTX6VEHJ4F"
+      };
+    
+    // firebase.initializeApp(firebaseConfig);
+
+    // Reference to the Firebase Realtime Database
+    var database = firebase.database();
+
+    // Function to add a new blog post
+    function addBlogPost(title, body) {
+        // Get a new key for the blog post
+        var newPostKey = firebase.database().ref().child('blog-posts').push().key;
+
+        // Create a new blog post object
+        var postData = {
+            title: title,
+            body: body
+        };
+
+        // Save the blog post data in the database
+        var updates = {};
+        updates['/blog-posts/' + newPostKey] = postData;
+
+        return firebase.database().ref().update(updates);
+    }
+
+    // Function to retrieve and display blog posts
+    function displayBlogPosts() {
+        var blogList = document.getElementById("blogList");
+
+        // Reference to the 'blog-posts' node in the database
+        var blogPostsRef = firebase.database().ref('blog-posts');
+
+        blogPostsRef.on('value', function(snapshot) {
+            blogList.innerHTML = ""; // Clear previous blog posts
+
+            snapshot.forEach(function(childSnapshot) {
+                var childData = childSnapshot.val();
+                var blogDiv = document.createElement("div");
+                blogDiv.innerHTML = "<h2>" + childData.title + "</h2><p>" + childData.body + "</p>";
+                blogList.appendChild(blogDiv);
+            });
+        });
+    }
+
+    // Event listener for the blog post form submission
+    document.getElementById("blogForm").addEventListener("submit", function(e) {
+        e.preventDefault();
+        var title = document.getElementById("title").value;
+        var body = document.getElementById("body").value;
+
+        addBlogPost(title, body)
+            .then(function() {
+                // Blog post added successfully, refresh the displayed blog posts
+                displayBlogPosts();
+            })
+            .catch(function(error) {
+                console.error("Error adding blog post: " + error.message);
+            });
+
+        // Clear form fields
+        document.getElementById("title").value = "";
+        document.getElementById("body").value = "";
+    });
+
+    // Call the function to display existing blog posts
+    displayBlogPosts();
